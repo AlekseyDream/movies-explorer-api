632966cfd8ebcf69f7e9fb0ca959126dcc4bcd89
AlekseyDream <u550@list.ru>
2023-07-31 23:50:47 +0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRouter = require('./users');
-const cardRouter = require('./movies');
+const movieRouter = require('./movies');
 const NotFoundError = require('../errors/NotFoundError');
 
-module.exports = router;
-
 router.use('/users', userRouter);
-router.use('/cards', cardRouter);
+router.use('/movies', movieRouter);
 router.use('*', (req, res, next) => next(new NotFoundError('Не найдено')));
+
+module.exports = router;
